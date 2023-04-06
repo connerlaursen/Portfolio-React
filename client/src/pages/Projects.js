8feafead93e7cf873034072460c830cc233c8b3f
Conner Laursen <conner@Conners-MacBook-Air.local>
2023-04-06 16:16:59 -0700

    finished project section

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -2,9 +2,46 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div className="projectWrapper">
+        <div className="projectWrapper">
+          <a href="https://github.com/wasmulb/WatchWorthy" className="projectPicture">
+            <div>
+              <h3>Project 1</h3>
+              <span>Watch Worthy</span>
+            </div>
+          </a>
+          <a href="https://wasmulb.github.io/RecGrabber/" className="projectPicture">
+            <div>
+              <h3>Project 2</h3>
+              <span>Rec Grabber</span>
+            </div>
+          </a>
+          <a href="https://blind-booking.herokuapp.com/" className="projectPicture">
+            <div>
+              <h3>Project 3</h3>
+              <span>Blind Booking</span>
+            </div>
+          </a>
+          <a href="https://github.com/connerlaursen/Social-Network-Api" className="projectPicture">
+            <div>
+              <h3>Project 4</h3>
+              <span>Social Network Api</span>
+            </div>
+          </a>
+          <a href="https://gist.github.com/connerlaursen/07c649fdb1161abb4530f0f5d41f771b" className="projectPicture">
+            <div>
+              <h3>Project 5</h3>
+              <span>Regex Tutorial</span>
+            </div>
+          </a>
+          <a href="https://connerlaursen-note-taker.herokuapp.com/" className="projectPicture">
+            <div>
+              <h3>Project 6</h3>
+              <span>Express Note Taker</span>
+            </div>
+          </a>
+      </div>
 
-    </div>
+      
   )
 }
 
