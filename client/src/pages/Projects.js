79a152f3129b108fc819217aea7c43eede940e7b
Conner Laursen <conner@Conners-MacBook-Air.local>
2023-04-17 18:22:25 -0700

    changed title and made projects open in new tab

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -4,37 +4,37 @@ function Projects() {
   return (
         <div className="projectWrapper">
           <div className ="projectSection">
-          <a href="https://github.com/wasmulb/WatchWorthy" className="projectPicture">
+          <a href="https://watch-worthy.herokuapp.com" target = "_blank" rel="noreferrer" className="projectPicture">
             <div>
               <h3 className = 'projectLabel'>Project 1</h3>
               <span className = 'projectName'>Watch Worthy</span>
             </div>
           </a>
-          <a href="https://wasmulb.github.io/RecGrabber/" className="projectPicture">
+          <a href="https://wasmulb.github.io/RecGrabber/" target = "_blank" rel="noreferrer" className="projectPicture">
             <div>
               <h3 className = 'projectLabel'>Project 2</h3>
               <span className = 'projectName'>Rec Grabber</span>
             </div>
           </a>
-          <a href="https://blind-booking.herokuapp.com/" className="projectPicture">
+          <a href="https://blind-booking.herokuapp.com/" target = "_blank" rel="noreferrer" className="projectPicture">
             <div>
               <h3 className = 'projectLabel'>Project 3</h3>
               <span className = 'projectName'>Blind Booking</span>
             </div>
           </a>
-          <a href="https://github.com/connerlaursen/Social-Network-Api" className="projectPicture">
+          <a href="https://github.com/connerlaursen/Social-Network-Api" target = "_blank" rel="noreferrer" className="projectPicture">
             <div>
               <h3 className = 'projectLabel'>Project 4</h3>
               <span className = 'projectName'>Social Network Api</span>
             </div>
           </a>
-          <a href="https://gist.github.com/connerlaursen/07c649fdb1161abb4530f0f5d41f771b" className="projectPicture">
+          <a href="https://gist.github.com/connerlaursen/07c649fdb1161abb4530f0f5d41f771b" target = "_blank" rel="noreferrer" className="projectPicture">
             <div>
               <h3 className = 'projectLabel'>Project 5</h3>
               <span className = 'projectName'>Regex Tutorial</span>
             </div>
           </a>
-          <a href="https://connerlaursen-note-taker.herokuapp.com/" className="projectPicture">
+          <a href="https://connerlaursen-note-taker.herokuapp.com/" target = "_blank" rel="noreferrer" className="projectPicture">
             <div>
               <h3 className = 'projectLabel'>Project 6</h3>
               <span className = 'projectName'>Express Note Taker</span>
