93af111cbd2e5743ce96ec59319680adcd8290aa
Conner Laursen <conner@Conners-MacBook-Air.local>
2023-05-19 16:36:43 -0700

    got contact form working and updated resume.pdf in assets

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,24 +1,55 @@
-import React from 'react'
+import React from 'react';
+import emailjs from 'emailjs-com';
+
+// Load EmailJS SDK
+(function () {
+  emailjs.init("ECMVldby_xx2D2VVz");
+})();
+
+function sendEmail(event) {
+  event.preventDefault();
+
+  // Get the form values using event.target.elements
+  var name = event.target.elements.name.value;
+  var email = event.target.elements.email.value;
+  var message = event.target.elements.message.value;
+
+  // Prepare the email parameters
+  var params = {
+    from_name: name,
+    from_email: email,
+    message: message,
+  };
+
+  // Send the email
+  emailjs.sendForm("service_4rqipdd", "template_2h5k9p8", event.target, "ECMVldby_xx2D2VVz")
+    .then(function (response) {
+      // Email sent successfully
+      alert("Email sent!");
+      // Reset the form
+      event.target.reset();
+    }, function (error) {
+      // An error occurred while sending the email
+      console.log("Error sending email:", error);
+    });
+}
 
 function Contact() {
   return (
     <div className="contactWrapper">
-       <div className="formcarry-container">
-      <form action="#" method="POST" className="formcarry-form">
+      <div className="formcarry-container">
+        <form onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder='Name' required />
 
-        <input type="text" id="name" name="fullName" placeholder='Name' required />
+          <input type="email" name="email" placeholder='Email Address' required />
 
-        <input type="email" id="email" name="email" placeholder='Email Address' required />
+          <textarea name="message" cols="30" rows="7" placeholder='Message'></textarea>
 
-       
-        <textarea name="message" id="message" cols="30" rows="7" placeholder='Message'></textarea>
-
-        <button type="submit">Send</button>
-      </form>
-    </div>
-
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   )
 }
 
-export default Contact
+export default Contact;
